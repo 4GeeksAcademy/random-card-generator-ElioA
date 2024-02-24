74cacd3fc59ac636a04c44537eb492b8e1c8c237
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-02-24 22:29:06 +0000

    my last commit

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ const numberBottom = document.getElementById("number-bottom");
 const centerFigure = document.getElementById("centerFigure");
 const topFigure = document.getElementById("figure-top");
 const bottomFigure = document.getElementById("figure-bottom");
+const myCard = document.getElementById("myCard");
+const cardWidth = document.getElementById("cardWidth");
+const cardHeight = document.getElementById("cardHeight");
 
 const numbersAndLetters = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 const figures = ["♠", "♣", "♥", "♦"];
@@ -21,6 +24,22 @@ window.onload = function() {
   console.log("Hello Rigo from the console!");
   button.addEventListener("click", getMyRandomCard);
   setInterval(getMyRandomCard, 10000);
+
+  myCard.style.height = cardHeight;
+  cardWidth.addEventListener("change", e => {
+    if (cardWidth.value == "") {
+      myCard.style.width = "500px";
+    } else {
+      myCard.style.width = cardWidth.value;
+    }
+  });
+  cardHeight.addEventListener("change", e => {
+    if (cardHeight.value == "") {
+      myCard.style.height = "600px";
+    } else {
+      myCard.style.height = cardHeight.value;
+    }
+  });
 };
 const randomIndex = arr => {
   return (arr = Math.floor(Math.random() * arr.length));
