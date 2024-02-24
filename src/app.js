21e0d8cbb4d586d966b29174404a148f4d052186
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-02-24 19:36:16 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const numberTop = document.getElementById("number-top");
+const numberBottom = document.getElementById("number-bottom");
+const centerFigure = document.getElementById("figure");
+const topFigure = document.getElementById("figure-top");
+const bottomFigure = document.getElementById("figure-bottom");
+const numbersAndLetters = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const figures = ["♠", "♣", "♥", "♦"];
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+const randomIndex = arr => {
+  return (arr = Math.floor(Math.random() * arr.length));
+};
+figure.innerHTML = figures[randomIndex(figures)];
+
+numberTop.innerHTML = numbersAndLetters[randomIndex(numbersAndLetters)];
+numberBottom.innerHTML = numberTop.innerHTML;
+centerFigure.innerHTML = figures[randomIndex(figures)];
+topFigure.innerHTML = centerFigure.innerHTML;
+bottomFigure.innerHTML = centerFigure.innerHTML;
+
+for (let item of figures) {
+  if (centerFigure.innerHTML === "♥" || centerFigure.innerHTML === "♦") {
+    numberTop.style.color = "red";
+    numberBottom.style.color = "red";
+    centerFigure.style.color = "red";
+    topFigure.style.color = "red";
+    bottomFigure.style.color = "red";
+  }
+}
